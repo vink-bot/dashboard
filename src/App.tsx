96f06d057483e850fc7cda0a4af21fb0ee4a8495
Dmitry <kfifa3@gmail.com>
2024-03-28 17:27:16 +0300

    feat: providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-  return <h2 className="font-black">HELLO</h2>;
-}
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<h2 className="font-black uppercase">hello</h2>} />
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/libs/store.ts
@@ -0,0 +1,20 @@
+import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+
+const rootReducer = combineSlices();
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export default store;
